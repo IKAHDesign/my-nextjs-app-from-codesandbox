32be6a2df8c5458f2d260d251eec2814681135e1
IKAH Design <ikahdesign@gmail.com>
2022-09-28 13:48:46 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Link href="/about">
+        <a>Me</a>
+      </Link>
     </div>
-  )
+  );
 }
